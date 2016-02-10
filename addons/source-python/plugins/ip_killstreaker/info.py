b4809fe56b92dfebe8dc889685551e0870967feb
KirillMysnik <kirill@mysnik.com>
2016-02-10 18:01:18 +0300

    Increased major version number

--- addons/source-python/plugins/ip_killstreaker/info.py
+++ addons/source-python/plugins/ip_killstreaker/info.py
@@ -6,7 +6,7 @@ info = PluginInfo()
 info.name = "iPlayer's Killstreaker"
 info.basename = 'ip_killstreaker'
 info.author = 'Kirill "iPlayer" Mysnik'
-info.version = '1.0'
+info.version = '2.0'
 info.variable = 'ipk_version'
 info.convar = PublicConVar(
     info.variable, info.version, "{} version".format(info.name))
